248bc0582df0345899914e865216b3c8c910bb78
Pedro Ramos <pedroramosromero@gmail.com>
2024-10-07 16:55:20 +0200

    Add verification of browser API calls

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -1,10 +1,27 @@
+/// <reference types="cypress" />
+
 describe('Test with backend', () => {
 
   beforeEach('login to application', () => {
     cy.loginToApplication()
   })
 
-  it('first test', () => {
-    cy.log('Yaaaaay, we logged in!')
+  it.only('verify correct request and response', () => {
+
+    cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/').as('postArticles')
+    
+    cy.contains('New Article').click()
+    cy.get('[formcontrolname="title"]').type('This is the title')
+    cy.get('[formcontrolname="description"]').type('This is a description')
+    cy.get('[formcontrolname="body"]').type('This is a body of the article')
+    cy.contains('Publish Article').click()
+    
+    cy.wait('@postArticles').then( xhr => {
+      console.log(xhr)
+      expect(xhr.response.statusCode).to.equal(201)
+      expect(xhr.request.body.article.body).to.equal('This is a body of the article')
+      expect(xhr.response.body.article.description).to.equal('This is a description')
+    })
+    
   })
 })
