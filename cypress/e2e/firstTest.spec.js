54b4be7764713ece61b2acaceb9922d4601c34d1
Pedro Ramos <pedroramosromero@gmail.com>
2024-10-07 18:49:50 +0200

    Add tests for mocking API responses

--- cypress/e2e/firstTest.spec.js
+++ cypress/e2e/firstTest.spec.js
@@ -3,10 +3,11 @@
 describe('Test with backend', () => {
 
   beforeEach('login to application', () => {
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/tags', {fixture: 'tags.json'})
     cy.loginToApplication()
   })
 
-  it.only('verify correct request and response', () => {
+  it('verify correct request and response', () => {
 
     cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/').as('postArticles')
     
@@ -24,4 +25,34 @@ describe('Test with backend', () => {
     })
     
   })
+
+  it('verify popular tags are displayed', () => {
+    cy.get('.sidebar').find('.tag-list')
+    .should('contain', 'cypress')
+    .and('contain', 'automation')
+    .and('contain', 'testing')
+  })
+
+  it.only('verify global feed likes count', () => {
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles/feed*', {"articles":[],"articlesCount":0})
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles*', { fixture: 'articles.json' })
+
+    cy.contains('Global Feed').click()
+    cy.get('app-article-list button').then(heartList => {
+        expect(heartList[0]).to.contain('1')
+        expect(heartList[1]).to.contain('5')
+    })
+
+    cy.fixture('articles.json').then(file => {
+      const articleLink = file.articles[1].slug
+      file.articles[1].favoritesCount = 6
+      cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/'+articleLink+'/favorite', file)
+    })
+    
+    cy.get('app-article-list button').eq(1).click().should('contain', '6')
+
+   })
+
+
+
 })
